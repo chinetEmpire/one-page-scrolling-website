c0c07f9574bab283a9bd4ca8f6fb02200674a1a4
chinetEmpire <chinet86@gmail.com>
2017-03-09 22:46:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chinet | Freelancer | Developer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
+	<link rel="stylesheet" type="text/css" href="css/custom.css"/>
+
+<link href="css/animate.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+	</head>
+
+<body> 
+
+
+
+<div id="wrapper">
+
+ <nav class="navbar navbar-default navbar-fixed-top " id="main-nav"> 
+<div class="container text-center">
+     
+     <div class="cd-bg-video-wrapper" data-video="img/home-hero">
+                                <!-- video element will be loaded using jQuery -->
+                            </div> <!-- .cd-bg-video-wrapper -->
+                            
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span> <span style="color: #fff;">Menu</span> <i class="fa fa-bars" style="color: #fff;"></i>
+                </button>
+                <a class="navbar-brand" data-toggle="modal" data-target="#smallModal"  >Freelance Developer</a>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li ><a id="1" href="#section"><i class="fa-home fa-1x" style="font-style: initial; color: #fff;"></i> hello</a></li>
+                     <li><a id="2" href="#section"><i class="  fa-server fa-1x" style="font-style: initial; color: #fff;"></i> services</a></li>
+					  <li><a id="3" href="#section"><i class=" fa-briefcase fa-1x" style="font-style: initial; color: #fff;"></i> portfolio</a></li>
+					  <li><a id="4" href="#section"><i class=" fa-envelope fa-1x" style="font-style: initial; color: #fff;"></i> contact</a></li>
+
+          
+                     
+                </ul>
+          
+           
+        </div>
+    </div></nav>
+
+
+
+<div class="home-sec .embed-responsive video " id="section1" data-vide-bg="img/Dark">
+
+
+<div class="overlay">
+
+	<div class="container text-center">
+<h1 style="font-family: roboto; color: #fff; text-align: center; padding-top: 200px; font-size: 30px;">Hello There,</h1>
+<h1 style="font-family: impact; color: #fff; text-align: center;  font-size: 60px;" class="wow bounceInLeft" data-wow-duration="3s">I'M CHINONSO</h1>
+<h1 style="font-family: impact; color: #fff; text-align: center;  font-size: 60px;" class="wow bounceInRight" data-wow-duration="4s"> A WEB DEVELOPER</h1>
+<h1 style="font-family: impact; color: yellow; text-align: center;  font-size: 20px;" class="wow tada" data-wow-duration="6s">I base in Lagos state, Nigeria.</h1><br>
+<a id="2" href="#section" class="btn btn-lg btn-primary" style="background: transparent;">NICE MEETING YOU</a><br>
+
+               
+                            
+	  
+</div>
+	
+</div>
+       </div>
+
+
+<div class="section" id="section2">
+
+<div class="row no-gutter">
+     	<div class="container-fluid"><br>
+
+     	<h1 style="color: #F05F40; text-align: center; font-family: font-family: 'Merriweather', 'Helvetica Neue', Arial, sans-serif;">WHAT I CAN DO</h1>
+     	<hr>
+     		<div class="col-lg-3 col-md-3 col-sm-3">
+     		<i class="fa-laptop fa-5x" style="color:#F05F40; font-size: 80px; margin-left: 120px; font-style: inherit; "></i>
+     		<h2 style="text-align: center;color:#F05F40; ">Web Development</h2><hr>
+<h4 style="color: #000; font-size: 18px; font-family: cursive; text-align: center;">I develop website for clients to suite their business.
+I make use of current frameworks and CMS tools to develop websites that are responsive in nature.</h4>
+     			
+     		</div>
+
+<div class="col-lg-3 col-md-3 col-sm-3">
+     			<i class="fa-cloud-upload fa-5x" style="color:#F05F40; font-size: 80px; margin-left: 120px; font-style: inherit; "></i>
+     		<h2 style="text-align: center;color:#F05F40; ">SEO expert</h2><hr>
+
+<h4 style="color: #000; font-size: 18px; font-family: cursive; text-align: center;">It is one thing to build a website and another thing to make the site visible in google search engine. A nice website without a good SEO is of no use.</h4>
+     			
+     		</div>
+
+<div class="col-lg-3 col-md-3 col-sm-3">
+     			<i class="fa-android fa-5x" style="color:#F05F40; font-size: 80px; margin-left: 120px; font-style: inherit; "></i>
+
+     			<h2 style="text-align: center;color:#F05F40; ">Mobile App Development</h2><hr>
+     		
+<h4 style="color: #000; font-size: 20px; font-family: cursive; text-align: center;">I develop Android application based on what the client needs.</h4>
+     			
+     		</div>
+<div class="col-lg-3 col-md-3 col-sm-3">
+     			<i class="fa-github fa-5x" style="color:#F05F40; font-size: 80px; margin-left: 120px; font-style: inherit; "></i>
+
+     			<h2 style="text-align: center;color:#F05F40; ">Training</h2><hr>
+     		
+<h4 style="color: #000; font-size: 20px; font-family: cursive; text-align: center;">I derive joy by training people who want to solve problems of the world using codes</h4><br>
+     			
+     		</div>
+
+
+     	</div></div></div>
+
+<div></div>
+
+<br>
+
+
+<div class="port" id="section3">
+    <div class="container-fluid">
+        <div class="row no-gutter">
+              
+                     <div class="col-lg-4"><img src="img/1.jpg" class="img-responsive"></div>
+                            <div class="col-lg-4"><img src="img/5.jpg" class="img-responsive"></div>
+                                 <div class="col-lg-4"><img src="img/6.jpg" class="img-responsive"></div>
+                                     <div class="col-lg-4"><img src="img/4.jpg" class="img-responsive"></div>
+                                             <div class="col-lg-4"><img src="img/2.jpg" class="img-responsive"></div>
+                                                 <div class="col-lg-4"><img src="img/3.jpg" class="img-responsive"></div>
+
+ 
+
+        </div>
+    </div>
+</div>
+
+<div id="section4">
+    <div class="overlay">
+        
+        <div class="row">
+
+
+<br>
+<br>
+<div class="col-lg-1 col-sm-1 col-md-1">
+  
+</div>
+
+<div class="col-md-10 col-lg-10 col-sm-10">
+  <div class="container-fluid">
+<h3 style="color: #fff; text-align: center;font-size: 40px;" class="wow bounceIn" wow-data-duration="3s">MESSAGE ME HERE</h3><hr>
+<input type="text" name="fullname" placeholder="full name" id="name" size="20" class="wow bounceInLeft" data-wow-duration="3s">
+<input type="email" name="email" placeholder="enter your email" id="name" size="20" class="wow bounceInUp" data-wow-duration="3s">
+<input type="text" name="subject" placeholder="subject" id="name" size="20" class="wow bounceInRight" data-wow-duration="3s"><br><br>
+<textarea  name="text" id="textarea" rows="10" cols="128" class="wow bounceIn" data-wow-duration="3s"></textarea><br>
+<input type="submit" name="submit" class="btn btn-success btn-lg" value="send" style="width: 100px;">
+
+</div>
+</div>
+
+
+<div class="col-lg-1 col-sm-1 col-md-1">
+  
+</div>
+
+
+
+
+    </div>
+
+
+   
+
+</div>
+
+
+</div>
+
+</div>
+
+
+
+
+
+ 
+       
+
+
+</body>
+
+ <!-- Small Modal -->
+        <div class="modal fade" id="smallModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Small Modal</h4>
+              </div>
+              <div class="modal-body">
+              <img src="img/profile.jpg" class="img-responsive">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+               
+              </div>
+            </div>
+         </div>
+
+<script src="js/wow.min.js"></script>
+<script>
+	new WOW().init();
+</script>
+<script src="js/jquery-1.10.2.js"></script>
+<script src="js/bootstrap.js"></script>
+<script  src="js/bootstrap.min.js"></script>
+     <!--  Scrolling Reveal Script -->
+    <script src="js/scrollReveal.js"></script>
+    <!--  Scroll Scripts --> 
+<script src="js/custom.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery-1.11.3-jquery.min.js"></script>
+<script src="js/jquery.vide.js"></script>
+
+</html>
